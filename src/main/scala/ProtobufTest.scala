93f1ccc95bad9993c4ed9ea6df6d9cba55189118
zhz55 <kasim@126.com>
2017-06-25 17:51:28 +0800

    protobuf

--- src/main/scala/ProtobufTest.scala
+++ src/main/scala/ProtobufTest.scala
@@ -1,6 +1,14 @@
 /**
   * Created by kasim on 2017/6/25.
   */
-object ProtobufTest {
+import SeaGoing.CTTIC.SEAGOING._
 
+object ProtobufTest{
+  def main(args: Array[String]): Unit = {
+
+    val tmp = Gnss()
+
+
+    println(tmp)
+  }
 }
